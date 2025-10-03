e8a27bd5cdfed9650c5b8d94be0b453985bbe30a
Mark Zou <markzou2@illinois.edu>
2025-10-03 15:54:34 +0000

    test_2

--- tests/tests.cc
+++ tests/tests.cc
@@ -49,6 +49,8 @@ TEST_CASE("Example: Create a new account", "[ex-1]") {
   REQUIRE(accounts.size() == 1);
   std::vector<std::string> empty;
   REQUIRE(transactions[{12345678, 1234}] == empty);
+  REQUIRE_THROWS_AS(atm.RegisterAccount(12345678, 1234, "Sam Sepiol", 300.30),
+                    std::invalid_argument);
 }
 
 TEST_CASE("Example: Simple widthdraw", "[ex-2]") {
